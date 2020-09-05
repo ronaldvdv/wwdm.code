dd33c2e905bac18cbe3112a01b2e7d0b3aadc6de
Ronald van der Velden <ronald.van.der.velden@gmail.com>
2020-09-05 19:06:20 +0200

    Using typescript

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -7,9 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-		<p>Code by Ronald</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"

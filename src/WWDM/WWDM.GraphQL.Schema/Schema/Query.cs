dc4e9e316d7333c08acb00513c121ded28af94e8
Ronald van der Velden <ronald.van.der.velden@gmail.com>
2020-09-06 22:31:06 +0200

    Adding root queries for Episode(id) and Season(id)

--- src/WWDM/WWDM.GraphQL.Schema/Schema/Query.cs
+++ src/WWDM/WWDM.GraphQL.Schema/Schema/Query.cs
@@ -7,8 +7,12 @@ namespace WWDM.GraphQL.Schema
 {
     public class Query
     {
+        public Task<Episode> Episode(int id, [Service] WWDMContext context) => context.Episodes.FirstOrDefaultAsync(e => e.Id == id);
+
         public Task<Season[]> GetSeasons([Service] WWDMContext context) => context.Seasons.ToArrayAsync();
 
         public string Hello() => "world";
+
+        public Task<Season> Season(int id, [Service] WWDMContext context) => context.Seasons.FirstOrDefaultAsync(s => s.Id == id);
     }
 }
--- src/WWDM/WWDM.GraphQL.Schema/Types/QueryType.cs
+++ src/WWDM/WWDM.GraphQL.Schema/Types/QueryType.cs
@@ -1,69 +1,15 @@
-﻿using System;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
-using HotChocolate.Resolvers;
-using HotChocolate.Types;
-using Microsoft.EntityFrameworkCore;
+﻿using HotChocolate.Types;
 using WWDM.GraphQL.Schema;
 using WWDM.Models;
 
 namespace WWDM.GraphQL.Types
 {
-    public static class ResolverExtensions
-    {
-        private static MethodInfo _containsMethod = typeof(Enumerable).GetRuntimeMethods().Single(m => m.Name == nameof(Enumerable.Contains) && m.GetParameters().Length == 2).MakeGenericMethod(typeof(int));
-
-        public static void ResolveOneToMany<TParent, TChild>(this IObjectFieldDescriptor parent, Expression<Func<TChild, int>> foreignKey, string keySuffix = "")
-            where TParent : IEntity
-            where TChild : class, IEntity
-        {
-            parent.Resolver((ctx, ct) =>
-            {
-                var set = ctx.Service<WWDMContext>().Set<TChild>();
-                var parentId = ctx.Parent<TParent>().Id;
-                var key = typeof(TParent).Name + "-to-" + typeof(TChild).Name + "-" + keySuffix;
-
-                return ctx.GroupDataLoader<int, TChild>(key, async keys =>
-                {
-                    var parameter = foreignKey.Parameters.Single();
-                    var subject = foreignKey.Body;
-                    var predicate = Expression.Lambda<Func<TChild, bool>>(Expression.Call(_containsMethod, Expression.Constant(keys), subject), parameter);
-                    var result = await set.Where(predicate).ToArrayAsync();
-                    var lookup = result.ToLookup(foreignKey.Compile());
-                    return lookup;
-                }).LoadAsync(parentId, ct);
-            });
-        }
-
-        public static void ResolveOneToOne<TParent, TChild>(this IObjectFieldDescriptor parent, Func<TParent, int> foreignKey, string keySuffix = "")
-            where TParent : IEntity
-            where TChild : class, IEntity
-        {
-            parent.Resolver((ctx, ct) =>
-            {
-                var set = ctx.Service<WWDMContext>().Set<TChild>();
-                var parent = ctx.Parent<TParent>();
-                var childId = foreignKey(parent);
-                var key = typeof(TParent).Name + "-to-" + typeof(TChild).Name + "-" + keySuffix;
-
-                return ctx.BatchDataLoader<int, TChild>(key, async keys =>
-                {
-                    var parameter = Expression.Parameter(typeof(TChild));
-                    var idExpression = Expression.Property(parameter, nameof(IEntity.Id));
-                    var predicate = Expression.Lambda<Func<TChild, bool>>(Expression.Call(_containsMethod, Expression.Constant(keys), idExpression), parameter);
-                    var result = await set.Where(predicate).ToDictionaryAsync(c => c.Id);
-                    return result;
-                }).LoadAsync(childId, ct);
-            });
-        }
-    }
-
     public class EpisodeType : ObjectType<Episode>
     {
         protected override void Configure(IObjectTypeDescriptor<Episode> descriptor)
         {
             base.Configure(descriptor);
+            descriptor.Field(e => e.Season).ResolveOneToOne<Episode, Season>(e => e.SeasonId);
             descriptor.Field(e => e.Image).ResolveOneToOne<Episode, Image>(e => e.ImageId);
         }
     }

9f1804914cfbd2b505c82198d3934a75df186065
Ronald van der Velden <ronald.van.der.velden@gmail.com>
2021-02-26 17:46:00 +0100

    Add missing root-level queries

--- src/WWDM/WWDM.GraphQL.Schema/Schema/Query.cs
+++ src/WWDM/WWDM.GraphQL.Schema/Schema/Query.cs
@@ -11,8 +11,18 @@ namespace WWDM.GraphQL.Schema
 
         public Task<Season[]> GetSeasons([Service] WWDMContext context) => context.Seasons.ToArrayAsync();
 
+        public Task<Episode[]> GetEpisodes([Service] WWDMContext context) => context.Episodes.ToArrayAsync();
+
+        public Task<Game[]> GetGames([Service] WWDMContext context) => context.Games.ToArrayAsync();
+
+        public Task<Participant[]> GetParticipants([Service] WWDMContext context) => context.Participants.ToArrayAsync();
+
         public string Hello() => "world";
 
         public Task<Season> Season(int id, [Service] WWDMContext context) => context.Seasons.FirstOrDefaultAsync(s => s.Id == id);
+
+        public Task<Game> Game(int id, [Service] WWDMContext context) => context.Games.FirstOrDefaultAsync(s => s.Id == id);
+
+        public Task<Participant> Participant(int id, [Service] WWDMContext context) => context.Participants.FirstOrDefaultAsync(s => s.Id == id);
     }
 }
